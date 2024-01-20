dc9d1cd378d5dd06f1213482cf363f3cc9886c05
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-01-20 21:54:35 +0530

    files are removed to upload latest  version

--- admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo '<script>window.location.assign("index.php");</script>';
-?>
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Cache-Control: no-cache');
-include 'element/connection.php';
-$spons = "119896";
-$pla_data=mysqli_fetch_array(mysqli_query($conn,"SELECT * FROM `users` WHERE `user_id`='$spons'"));
-  if($pla_data['left_side']!="" && $pla_data['right_side']!=""){
-      echo  "pair generated";
-  }
