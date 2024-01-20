5406784eef97d275d3d0d1a43666881c25cbc4a3
Muhammad Ashif Raza <110925648+mdashifraza7786@users.noreply.github.com>
2024-01-20 22:59:16 +0530

    Page title updated

--- withdraw.php
+++ withdraw.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title> Withdraw Reques</title>
+    <title> Withdraw Request</title>
     <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
 
     <!-- project css file  -->
